a4f6cf3b2e79cc483a51ed983e70283e01b3306b
Adarsh-S-Nair <asnair159@gmail.com>
2025-06-14 19:27:38 -0400

    update production url in allowed origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",  # dev
-        "https://trading-simulator-htnq.onrender.com",  # production
+        "https://zentari-7opu.onrender.com",  # production
     ],
     allow_credentials=True,
     allow_methods=["*"],
