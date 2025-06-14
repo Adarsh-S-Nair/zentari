881414932e33e62b5671b3eb261f4d48f2326b30
Adarsh-S-Nair <asnair159@gmail.com>
2025-06-14 19:12:17 -0400

    allow production origin

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",  # dev
+        "https://trading-simulator-htnq.onrender.com",  # production
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
